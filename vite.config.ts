a051ae4802286fef711bdc488b11783e71461f32
PedroGomes <pedro.gomes@twoimpulse.com>
2024-01-11 23:30:25 +0000

    refactor: add tailwind. update configs

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		host: true,
+		host: false,
 		port: 4000,
 		watch: {
 			usePolling: true,
