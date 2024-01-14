d31ed5f0ac0f58fe3f22e42b7f03761bf725a677
PedroGomes <pedro.gomes@twoimpulse.com>
2024-01-14 20:23:16 +0000

    refactor: scrollable main content

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -13,19 +13,19 @@ export default function AppLayout(props: AppLayourProps) {
   const { children } = props;
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col h-screen">
       <HeaderSection>
         <div className="flex flex-row items-center gap-2">
           <Dribbble size={32} strokeWidth={2.25} />
           <h3 className="font-bold text-black text-xl">NBA Stats</h3>
         </div>
       </HeaderSection>
-      <section className="flex flex-row flex-grow">
+      <aside className="flex flex-row flex-1 overflow-hidden">
         <NavbarSection>
           <Navbar />
         </NavbarSection>
         <MainSection>{children}</MainSection>
-      </section>
+      </aside>
     </div>
   );
 }
--- src/components/lib/Layout/MainLayout.tsx
+++ src/components/lib/Layout/MainLayout.tsx
@@ -4,5 +4,5 @@ interface MainLayoutProps {
 
 export default function MainLayout(props: MainLayoutProps) {
   const { children } = props;
-  return <main className="w-full p-4 bg-[#3d3d3c]/5">{children}</main>;
+  return <main className="flex-1 overflow-y-auto w-full p-4 bg-[#3d3d3c]/5">{children}</main>;
 }
